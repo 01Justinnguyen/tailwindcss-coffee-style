19c9ee7cc22fc7aa7dd346c9abbfa1a0db0fdec9
Liam <phuc01112002@gmail.com>
2023-02-15 18:08:17 +0700

    adđ featured mugs and product card and responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       fontFamily: {
         Karla: ['Karla', 'sans-serif'],
       },
+
       colors: {
         'light-coffee': '#c89f94',
         coffee: {
@@ -15,14 +16,21 @@ module.exports = {
           600: '#744838',
         },
       },
+
       keyframes: {
         slideDown: {
           '0%': { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(0)' },
         },
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
       },
+
       animation: {
         slideDown: 'slideDown 0.4s ease-in-out',
+        fadeIn: 'fadeIn 0.5s ease-in-out',
       },
 
       backgroundImage: {
